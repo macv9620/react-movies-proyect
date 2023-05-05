59950dd8fb45374e9e8be6871a16852d78920b38
Mateo Vasco <macv9620@utp.edu.co>
2023-05-04 23:23:01 -0500

    Se estructura componente  header del proyecto

---
+++ src/App/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import { Header } from '../Components/Header/Header.jsx'
+
+function App() {
+  return(
+    <Header/>
+  )
+}
+
+export {App}
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import "./header.css";
+import logoIcon from "../../assets/drama.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-info">
+        <p className="header-info-welcome">Your-Show</p>
+
+      </div>
+      <div className="header-logo">
+        <img src={logoIcon} className="logo" />
+      </div>
+      <div className="header-search-bar">
+        <input
+          placeholder="Type to Search"
+          className="header-search-bar-input"
+          type="text"
+        />
+        <button className="header-search-bar-button">
+          <span className="material-symbols-outlined">search</span>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export { Header };
