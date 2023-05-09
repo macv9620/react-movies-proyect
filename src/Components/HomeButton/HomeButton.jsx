8001a5ff19157243306fa8368d3a49004643f60f
Mateo Vasco <macv9620@utp.edu.co>
2023-05-09 18:13:55 -0500

    Se implementa renderizado condicional del botón Home

--- src/Components/HomeButton/HomeButton.jsx
+++ src/Components/HomeButton/HomeButton.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './HomeButton.css'
 import homeIcon from '../../assets/home.png'
 
 const HomeButton = () => {
-  return (
-    <button
-      className='TodoButtonHome'
-    >
-      <Link to='/'>
-        <img className='home-icon' src={homeIcon} />
-      </Link>
-    </button>
-  )
+  const location = useLocation()
+  console.log(location)
+  if (location.pathname !== '/') {
+    return (
+      <button
+        className='TodoButtonHome'
+      >
+        <Link to='/'>
+          <img className='home-icon' src={homeIcon} />
+        </Link>
+      </button>
+    )
+  }
 }
 
 export { HomeButton }
